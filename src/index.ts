c79ed7099e35d119efd23d6957a7d7f1fad90017
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-05-30 00:25:27 +0545

    style: Update code formatting in .prettierrc, src/db.ts, and vitest.config.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 // import { db } from "./db";
 // import { sql } from "drizzle-orm";
-
 // const query = sql`select "hello world" as text`;
 // const result = db.get<{ text: string }>(query);
 // console.log(result);
-
-import { db } from "./db";
-import * as schema from "./schema";
+import { db } from './db';
+import * as schema from './schema';
 
 const result = await db.select().from(schema.movies);
 console.table(result);
