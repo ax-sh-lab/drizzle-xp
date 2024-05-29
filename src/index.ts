93afb8da0be594866074354b186ca3e2042700bd
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-05-29 20:08:03 +0545

    feat: improved command to show inserts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
-import { db } from "./db";
-import { sql } from "drizzle-orm";
+// import { db } from "./db";
+// import { sql } from "drizzle-orm";
 
-const query = sql`select "hello world" as text`;
-const result = db.get<{ text: string }>(query);
-console.log(result);
+// const query = sql`select "hello world" as text`;
+// const result = db.get<{ text: string }>(query);
+// console.log(result);
+
+import { db } from "./db";
+import * as schema from "./schema";
+
+
+const result = await db.select().from(schema.movies);
+console.table(result);
