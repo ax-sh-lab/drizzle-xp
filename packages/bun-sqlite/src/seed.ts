9c2caf2a558c5569237086977387f716d8e85d23
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-05-30 01:15:22 +0545

    feat: Add random movie generation to seed script

--- packages/bun-sqlite/src/seed.ts
+++ packages/bun-sqlite/src/seed.ts
@@ -1,5 +1,20 @@
+import { faker } from '@faker-js/faker';
+
 import { db } from './db';
 import * as schema from './schema';
+
+type Movie = { title: string; releaseYear: number };
+
+function createRandomMovie(): Movie {
+  return {
+    title: faker.internet.userName(),
+    releaseYear: faker.date.anytime().getFullYear()
+  };
+}
+
+export const Movies: Movie[] = faker.helpers.multiple(createRandomMovie, {
+  count: 5
+});
 
 await db.insert(schema.movies).values([
   {
@@ -13,7 +28,8 @@ await db.insert(schema.movies).values([
   {
     title: 'The Matrix Revolutions',
     releaseYear: 2003
-  }
+  },
+  ...Movies
 ]);
 
 console.log(`Seeding complete.`);
