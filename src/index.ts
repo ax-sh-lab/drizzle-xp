64a7bc959e170e361018d6673083278a9a530bc7
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-05-30 00:42:57 +0545

    refactor: Update in-memory database testing and query method in db-mem.test.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
-// import { db } from "./db";
-// import { sql } from "drizzle-orm";
-// const query = sql`select "hello world" as text`;
-// const result = db.get<{ text: string }>(query);
-// console.log(result);
 import { db } from './db/db.ts';
 import * as schema from './db/schema.ts';
 
