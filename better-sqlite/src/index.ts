fb29362061701928f791535fd5799dccd64bb8bc
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-05-30 01:01:20 +0545

    feat: wip

--- better-sqlite/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { db } from './db/db.ts';
-import * as schema from './db/schema.ts';
-
-async function main() {
-  const result = await db.select().from(schema.movies);
-  console.table(result);
-}
-void main();
